940c054c1e8888b2662da5169b22dc227a18fe0c
kennedykiptum <kennedykiptum76@gmail.com>
2022-06-02 12:24:21 -0700

    i Ple C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new linease enter the commit message for your changes. Lines starting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  *
@@ -10,7 +10,8 @@
  *
  *     */
 int main (void){
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0)
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 
 }
