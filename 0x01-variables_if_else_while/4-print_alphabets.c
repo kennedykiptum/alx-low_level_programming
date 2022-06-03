70f8d9752df3c52f42136bcbdf1e94869ac3ad66
kennedykiptum <kennedykiptum76@gmail.com>
2022-06-03 09:58:32 -0700

    Write a program that prints the alphabet in lowercase, followed by a new line.
    Print all the letters except q and e

--- 0x01-variables_if_else_while/4-print_alphabets.c
+++ 0x01-variables_if_else_while/4-print_alphabets.c
@@ -7,9 +7,14 @@ int main(void)
 {
 	char ch = 'a';
 
-	for (ch  = 'a'; ch <= 'z'; ch++)
-	if (ch != 'e' && ch != 'q')
-		putchar(ch);
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
 	putchar('\n');
 	return (0);
 
