cc2504e218ac2e61c25b286b6246fffa3249a038
kennedykiptum <kennedykiptum76@gmail.com>
2022-06-03 11:01:32 -0700

    rite a program that prints all possible combinations of single-digit number

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		putchar(c);
+		if (c != 60)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
