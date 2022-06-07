accfc5ee79d6be4fbaad1ebd0f9bddde00edbac8
kennedykiptum <kennedykiptum76@gmail.com>
2022-06-07 04:32:24 -0700

    a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -16,5 +16,6 @@ int _putchar(char c)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+	return (0);
 
 }
