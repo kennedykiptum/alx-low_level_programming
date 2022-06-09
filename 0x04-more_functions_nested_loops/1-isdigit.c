c0a43bd3f144b10f5e0c0375d3aa2319a6a99d1d
kennedykiptum <kennedykiptum76@gmail.com>
2022-06-09 04:54:08 -0700

    a function that checks for a digit (0 through 9).

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 
 /**
  * _isdigit - checks for a digit (0 through 9).
- * @c: the number to be checked
+ * @c: int the number to be checked
  * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
