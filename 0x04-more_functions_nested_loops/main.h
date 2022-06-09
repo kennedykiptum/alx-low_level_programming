8b897ed8ece407761f2acd4d7ede388d36f5836d
kennedykiptum <kennedykiptum76@gmail.com>
2022-06-09 10:09:15 -0700

    correcrion

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,6 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
-
+#include <stdio.h>
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
-
-#endif
