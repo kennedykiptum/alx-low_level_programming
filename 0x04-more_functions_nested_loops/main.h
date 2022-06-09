7ef05dbd75f16f42dc73d1cc6c1efa670bee5043
kennedykiptum <kennedykiptum76@gmail.com>
2022-06-09 07:07:52 -0700

    modified main

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,10 @@
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+
+#endif
