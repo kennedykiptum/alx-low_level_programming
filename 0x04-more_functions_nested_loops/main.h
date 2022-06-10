2d347faa97c1993855955c23f2c11c41c4f7d16c
kennedykiptum <kennedykiptum76@gmail.com>
2022-06-10 13:11:27 -0700

    changes

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,7 @@
-#include <stdio.h>
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-int main(void);
 void print_numbers(void);
 void print_most_numbers(void);
 void print_most_numbers(void);
