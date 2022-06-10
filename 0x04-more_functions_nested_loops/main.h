7bc7cf6402b9716b4de4c57f24dd6db85705253f
kennedykiptum <kennedykiptum76@gmail.com>
2022-06-10 13:15:50 -0700

    chan

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -12,3 +14,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
+#endif
