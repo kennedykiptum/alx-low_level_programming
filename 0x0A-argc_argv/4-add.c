b024716848a6107d64478c6e6a1ce01988deba57
kennedykiptum <kennedykiptum76@gmail.com>
2022-06-26 03:36:47 -0700

    program that multiplies two numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,18 +10,24 @@
  * @argc: counts numbe of args passed
  * Return: return 0
  */
+int atoi(int i);
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i;
+	int sum;
+
+	sum = 0;
 
 	if (argc == 1)
 		printf("0\n");
 	for (i = 1; i < argc; i++)
 	{
-		/**if (argv[i] > '9' || argv[i] < '0')
-			printf("Error\n");
-		return (1);*/
-		sum += atoi(argv[i]);
+/**
+ * if (argv[i] > '9' || argv[i] < '0')
+ * printf("Error\n");
+ * return (1);
+ */
+		sum = sum + atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
