20a6c63aeb2c72ff1c7597797d3bb3a0683d7026
kennedykiptum <kennedykiptum76@gmail.com>
2022-07-04 09:57:22 -0700

    a new type struct dog with the following elements:

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - gives information
+ * @name: the name of dog
+ * @age: dogs age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age:
+	char *owner;	
+};
+#endif
